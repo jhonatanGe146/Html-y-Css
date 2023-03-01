bf3ba8aa77c92b634f41e145a0a8640c97376af3
jhonatanGe146 <jhonatangelvis@gmail.com>
2023-03-01 07:55:42 -0500

    first commit

---
+++ clases/clases.php
@@ -0,0 +1,5 @@
+<?php
+?>
+<h1> Estoy en el archivo de clases</h1>
+<?php
+?>
---
+++ principal.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Aprende ahora!!!!</title>
+    
+</head>
+<body>
+    <header>
+        <div class="cab_izq"> 
+            <div class="izquierda">
+                <div class="img">
+                    <img src="imgs/logo1.jpg" alt="">
+                </div>
+                <div class="derecha"> 
+                    <img src="imgs\logo2.jpg" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="cab_cent" >
+        <p> <strong> HTML Y CSS </strong></p>
+        <br>
+        <p>HTML (HyperText Markup Language) y CSS (Cascading Style Sheets) son dos lenguajes fundamentales para la construcción y diseño de sitios web.</p>
+
+        </div>
+
+        <div class="cab_der"> <center><h3>Código de HTML Y CSS </h3>
+            <img src="imgs\cod.jpg" alt=""></center>
+    
+        </div> 
+
+    
+    </header>
+    </div>
+    <nav> 
+        <div class="img_nav">
+            <img src="imgs/pano.jpg" alt="">
+
+
+
+        </div>
+        <ul>
+            <li>
+                <a href="principal ">Inicio</a>
+                <a href="clases/clases.php">Facturación</a>
+                <a href="">Ventas</a>
+                <a href="quejas_reclamos.php">PQRDS</a>
+                <a href="Presentación.php">Ingreso</a>
+                
+            </li>
+        </ul>
+
+
+
+    </nav>
+
+
+
+
+
+    <aside> </aside>
+    
+    <section> </section>
+    
+    <footer> </footer>
+    
+</body>
+</html>
+
+
+

fd192c97b611659f2364c2caecde172a445e6117
jhonatanGe146 <jhonatangelvis@gmail.com>
2023-03-07 11:31:21 -0500

    hotel

---
+++ Hotel_Perseo.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Administración Habitación</title>
+    <link rel="stylesheet" href="estilos/Style.css">
+</head>
+<body>
+    <header>
+        <div class="Cab_Der">
+            <img src="Imgs_Hp" alt="">
+        </div>
+        <div class="Cab_Cent"> 
+
+        </div>
+        <div class="Cab_Izq">
+
+        </div>
+ 
+    </header>
+    <nav>
+
+    </nav>
+    <section>
+        <div class="Info_hab"> 
+            
+        </div>
+
+    </section>
+    <aside>
+
+    </aside>
+    <footer>
+
+    </footer>    
+</body>
+</html>
